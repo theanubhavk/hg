11c7b8ba0bd1c4acff9570f3ba7aec4d04953c04
Anubhav <85733533+theanubhavk@users.noreply.github.com>
2026-08-30 18:27:25 +0530

    Add start script for Honeygain and web server

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# 1. Start Honeygain in the background
+# (Make sure to pass your variables from Render's Environment Variables tab)
+/app/honeygain -tou-accept -email "$HG_EMAIL" -pass "$HG_PASSWORD" -device "$HG_DEVICE" &
+
+# 2. Start a simple Python web server on port 10000 to trick Render's health check
+python3 -m http.server 10000
